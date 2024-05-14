244d9a828483384d3fde298eea96cc66e3662197
Carolina Viero <carolinaviero1@gmail.com>
2024-05-14 16:05:15 +0100

    Live coding from Office Hours week 6

---
+++ week-6/characters.js
@@ -0,0 +1,17 @@
+const characters = [
+  {
+    name: 'Carrie Bradshaw',
+    imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThUG1r1alIf3EyTX1VayHSYCF7uEXJZey6MhxG-d2jHc6_Qt8G7L2CPSBViNkdfXsM_rw&usqp=CAU',
+    altText: 'Carrie Bradshaw'
+  },
+  {
+    name: 'Stanford Blatch',
+    imageUrl: 'https://media-cldnry.s-nbcnews.com/image/upload/t_fit-760w,f_auto,q_auto:best/newscms/2021_38/1778385/willie-garson-satc-kb-inline-210922.jpg',
+    altText: 'Stanford Blatch'
+  },
+  {
+    name: 'Samantha Jones',
+    imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9OrimByGBnAhXQe8eIO780L4pt9m21tJe03ejLsbpaQ&s',
+    altText: 'Samantha Jones'
+  },
+];
